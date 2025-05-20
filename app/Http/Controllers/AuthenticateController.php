35b72edda09e1f4190be1bb57ae99f83e966373b
marilinc27 <marilincastillo-@hotmail.com>
2025-05-20 20:28:03 -0300

    login

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+use Hash;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash as FacadesHash;
+use Illuminate\Support\Facades\Session as FacadesSession;
+
+class AuthenticateController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function registration(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postLogin(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('home')
+                ->withSuccess('Sesion inicada correctamente');
+        }
+
+        return redirect("/")->withError('Cualquier cosa le mandaste!!!!');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postRegistration(StoreUserRequest $request): RedirectResponse
+    {
+        $validatedData = $request->validated();
+
+
+
+        $user = $this->create($validatedData);
+
+        Auth::login($user);
+
+        return redirect("home")->withSuccess('Todo ok!');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function dashboard()
+    {
+        if (Auth::check()) {
+            return view('home');
+        }
+
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => FacadesHash::make($data['password'])
+        ]);
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function logout(): RedirectResponse
+    {
+        FacadesSession::flush();
+        Auth::logout();
+
+        return Redirect('/');
+    }
+}
