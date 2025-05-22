b87a2cd2db4dbe12be5f9c9eecb83e1a7d111981
marilinc27 <marilincastillo-@hotmail.com>
2025-05-22 15:21:56 -0300

    migración estados

---
+++ database/migrations/2025_05_22_181356_estados.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estados', function (Blueprint $table) {
+            $table->id();
+            $table->string('estado');
+            $table->timestamps();
+        });
+
+         DB::table('estados')->insert([
+            ['estado' => 'Activo'],
+            ['estado' => 'Baja'],
+            ['estado' => 'Habilitado'],
+            ['estado' => 'Deshabilitado'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
