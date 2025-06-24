1172b7bfda35e65e451753eb9cb06f3f9b07771e
marilinc27 <marilincastillo-@hotmail.com>
2025-06-23 23:17:59 -0300

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="display-5 fw-bold">Hola, {{ auth()->user()->name }}</h1>
-<button class="btn btn-primary btn-lg" type="button">Dashboard</button>
+<div class="container py-4">
+    <div class="row align-items-center mb-4">
+        <div class="col-md-8">
+            <h1 class="display-5 fw-bold">Hola, {{ auth()->user()->name }}</h1>
+            <p class="lead">Bienvenido/a a tu panel de control. Desde aquí podés acceder a las funciones principales del sistema.</p>
+        </div>
+        <div class="col-md-4 text-center">
+            <!-- Espacio para imagen de usuario o ilustración -->
+            <img src="{{ asset('img/img-bus.jpg') }}" alt="Dashboard" class="img-fluid rounded" style="max-height: 200px;">
+        </div>
+    </div>
 @endsection
