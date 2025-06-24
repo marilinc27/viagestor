ce7f88c6d41f33ab6e8208655247107d449cbcce
marilinc27 <marilincastillo-@hotmail.com>
2025-06-24 14:22:09 -0300

    tablas

--- public/js/tablas/empleados.js
+++ public/js/tablas/empleados.js
@@ -1,90 +1,90 @@
 $(document).ready(function () {
-    let table = new DataTable('#tablaempleados', {
+    let table = new DataTable("#tablaempleados", {
+        info: false,
         processing: true,
         serverSide: false,
+        lengthChange: false,
+        paging: true,
         language: {
-            url: 'languaje/espanoltabla.json'
+            url: "languaje/espanoltabla.json",
         },
         ajax: {
             url: urlDatosEmpleado,
-            type: 'POST',
+            type: "POST",
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')  // Se añade el token CSRF
-            }
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"), // Se añade el token CSRF
+            },
         },
-        type: 'post',
+        type: "post",
         columns: [
-            { data: 'id', visible: false },
-            { data: 'name', title: 'Nombre y apellido' },
-            { data: 'email', title: 'Correo' },
-            { data: 'tipo', title: 'Tipo' },
+            { data: "id", visible: false },
+            { data: "name", title: "Nombre y apellido" },
+            { data: "email", title: "Correo" },
+            { data: "tipo", title: "Tipo" },
             {
                 data: null,
-                title: 'Acciones',
+                title: "Acciones",
                 orderable: false,
                 searchable: false,
                 render: function (data, type, row) {
                     console.log(data);
-                    if (row.tipo == 'VENDEDOR') {
+                    if (row.tipo == "VENDEDOR") {
                         if (row.estado == 2) {
                             return `<button class="btn btn-sm btn-danger btn-borrar" data-id="${row.id}">🗑️ Borrar</button>`;
                         } else {
                             return `<button class="btn btn-sm btn-success btn-reactivar" data-id="${row.id}">♻️ Reactivar</button>`;
                         }
                     }
-                    return '';
-
-                }
-            }
-        ]
-
+                    return "";
+                },
+            },
+        ],
     });
 });
 
+$("#tablaempleados").on("click", ".btn-borrar", function () {
+    const id = $(this).data("id");
 
-$('#tablaempleados').on('click', '.btn-borrar', function () {
-    const id = $(this).data('id');
-
-    if (confirm('¿Estás seguro de que querés dar de baja este empleado?')) {
+    if (confirm("¿Estás seguro de que querés dar de baja este empleado?")) {
         fetch(`${baseUrlEdit}/${id}`, {
-            method: 'DELETE',
+            method: "DELETE",
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
         })
-            .then(response => response.json())
-            .then(data => {
+            .then((response) => response.json())
+            .then((data) => {
                 if (data.success) {
                     alert(data.message);
-                    $('#tablaempleados').DataTable().ajax.reload();
+                    $("#tablaempleados").DataTable().ajax.reload();
                 } else {
-                    alert('Ocurrió un error.');
+                    alert("Ocurrió un error.");
                 }
             });
     }
 });
 
-$('#tablaempleados').on('click', '.btn-reactivar', function () {
-    const id = $(this).data('id');
+$("#tablaempleados").on("click", ".btn-reactivar", function () {
+    const id = $(this).data("id");
 
-    if (confirm('¿Querés reactivar este empleado?')) {
+    if (confirm("¿Querés reactivar este empleado?")) {
         fetch(`${baseUrlEdit}/reactivar/${id}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
         })
-            .then(response => response.json())
-            .then(data => {
+            .then((response) => response.json())
+            .then((data) => {
                 if (data.success) {
                     alert(data.message);
-                    $('#tablaempleados').DataTable().ajax.reload();
+                    $("#tablaempleados").DataTable().ajax.reload();
                 } else {
-                    alert('Ocurrió un error.');
+                    alert("Ocurrió un error.");
                 }
             });
     }
--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
-
+<link rel="stylesheet" href="css/tablas.css" />
 @section('content')
     <section>
 
         <a href="{{ route('empleados.create') }}" class="btn btn-primary mb-5">Crear Empleado</a>
-        <table id="tablaempleados" class="display table table-bordered">
+        <table id="tablaempleados" class="display table display table-custom">
             <thead>
+                <tr class="header-principal">
+                    <th>ID</th>
+                    <th>Nombre y apellido</th>
+                    <th>Correo</th>
+                    <th>Tipo</th>
+                    <th>Acciones</th>
+                </tr>
 
-                <th>ID</th>
-                <th>Nombre y apellido</th>
-                <th>Correo</th>
-                <th>Tipo</th>
-                <th>Acciones</th>
             </thead>
 
             <tbody>
